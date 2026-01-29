28b1f8c4e74835c0b593c82e6e5544bdd368c3dc
Giovanni Martins <giovannidealmeidamartins@gmail.com>
2026-01-29 20:46:41 -0300

    fix: put strip in the final of rabbit url

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ import json
 
 class Constants():
     def __init__(self):
-        self.RABBITMQ_URL = os.getenv('RABBITMQ_URL', 'amqp://guest:guest@localhost:5672//')
+        self.RABBITMQ_URL = os.getenv('RABBITMQ_URL', 'amqp://guest:guest@localhost:5672//').strip()
         self.MAIN_EXCHANGE = 'app_events_exchange'
         self.DLX_EXCHANGE = 'app_events_dlx'
         self.QUEUE_MAP = self._load_mapping()
