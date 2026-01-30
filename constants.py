76ef359afbd3ba0f95ca7529a16ea86c1ce02e2f
Giovanni Martins <giovannidealmeidamartins@gmail.com>
2026-01-29 21:19:17 -0300

    chore: update hash

--- constants.py
+++ constants.py
@@ -8,7 +8,7 @@ class Constants():
         self.RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'rabbitmq')
         self.RABBITMQ_PORT = os.getenv('RABBITMQ_PORT', '5672')
         self.RABBITMQ_USER = os.getenv('RABBITMQ_USER', 'guest')
-        self.RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD', 'guest')
+        self.RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD', 'guest') 
         self.RABBITMQ_URL = f"amqp://{self.RABBITMQ_USER}:{self.RABBITMQ_PASSWORD}@{self.RABBITMQ_HOST}:{self.RABBITMQ_PORT}"
         self.MAIN_EXCHANGE = 'app_events_exchange'
         self.DLX_EXCHANGE = 'app_events_dlx'
